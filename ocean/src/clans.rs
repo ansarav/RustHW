69250b5a31ffc5ed0d4108388c1ab5a38aa3faa1
ansarav <76548988+ansarav@users.noreply.github.com>
2024-06-13 17:14:59 -0700

    clan

--- ocean/src/clans.rs
+++ ocean/src/clans.rs
@@ -1,39 +1,60 @@
 
 #[derive(Debug)]
 pub struct ClanSystem {
-    // TODO: add necessary fields
+    clans : HashMap<String, HashSet<String>>,
 }
 
 impl ClanSystem {
     pub fn new() -> ClanSystem {
-        unimplemented!();
+        ClanSystem{
+            clans: HashMap::new(),
+        }
     }
 
     /**
      * Returns a list of the names of the clan members for the given clan id.
      */
     pub fn get_clan_member_names(&self, clan_id: &str) -> Vec<String> {
-        unimplemented!();
+        //if let
+        match self.clans.get(clan_id){
+            Some(member) => member.clone(),
+            None => Vec ::new(),
+        }
     }
 
     /**
      * Returns the number of clans currently in existence.
      */
     pub fn get_clan_count(&self) -> usize {
-        unimplemented!();
+        self.clans.len()
     }
 
     /**
      * Returns the number of clan members for the given clan id.
      */
     pub fn get_clan_member_count(&self, clan_id: &str) -> usize {
-        unimplemented!();
+        match self.clans.get(clan_id){
+            Some(member) => member.len(),
+            None => 0
+        }
     }
 
     /**
      * Returns the id of the clan with the most number of members, or None if such a clan does not exist.
      */
     pub fn get_largest_clan_id(&self) -> Option<String> {
-        unimplemented!();
+        //similar format to get_fastest_crab
+        let mut largets_clan_id = None;
+        let largest_size = 0 ;
+
+        for (clan_id ,members) in &self.clans{
+            let mem_count = members.len();
+            if mem_count > largets_clan_id {
+                largets_clan_id = Some(clan_id.clone());
+                largest_size = mem_count
+            }
+        }
+
+        largets_clan_id
     }
 }
